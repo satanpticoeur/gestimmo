4dfdfe30cbb0fa941a47cf9ee3fa3cee9f897d21
satanpticoeur <121906238+satanpticoeur@users.noreply.github.com>
2025-01-08 20:26:41 +0000

    Update page.tsx

--- app/announcements/[id]/edit/page.tsx
+++ app/announcements/[id]/edit/page.tsx
@@ -11,6 +11,8 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { announcementSchema } from "@/yup/schema";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 type Params = Promise<{ id: string }>;
 
 export default function EditAnnouncementPage(props: { params: Params }) {
@@ -35,7 +37,7 @@ export default function EditAnnouncementPage(props: { params: Params }) {
   useEffect(() => {
     const fetchAnnouncement = async () => {
       try {
-        const response = await fetch(`/api/announcement/${params.id}`)
+        const response = await fetch(`/api/announcement/${params.id}`);
         const data = await response.json();
 
         setValue("title", data.title);
@@ -58,10 +60,9 @@ export default function EditAnnouncementPage(props: { params: Params }) {
             }))
           );
         }
-        
       } catch (error) {
         console.error("Erreur lors du chargement de l'annonce:", error);
-        router.push("/announcements");
+        toast.error("Failed to load announcement");
       }
     };
 
@@ -114,120 +115,126 @@ export default function EditAnnouncementPage(props: { params: Params }) {
         method: "PUT",
         body: formData,
       });
-      
+
       if (response.ok) {
         router.push(`/announcements/${params.id}`);
+        toast.success("Announcement updated successfully");
       } else {
         console.error("Erreur lors de la mise à jour de l'annonce:", response);
+        toast.error("Failed to update announcement");
       }
     } catch (error) {
       console.error("Erreur lors de la mise à jour de l'annonce:", error);
+      toast.error("Failed to update announcement");
     }
   });
 
   return (
-    <form
-      method="post"
-      onSubmit={onSubmit}
-      encType="multipart/form-data"
-      className="max-[812px]:px-4"
-    >
-      <div className="border rounded-md text-muted-foreground p-4">
-        <header className="border-b pb-4 mb-10">
-          <h2 className="text-2xl font-semibold ">Announcement Details</h2>
-        </header>
-        <div className="flex flex-col gap-4 ">
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-6">
-            <InputDetail
-              label="Title"
-              register={register("title")}
-              errors={errors.title}
-            />
-            <InputDetail
-              label="Description"
-              register={register("description")}
-              errors={errors.description}
-            />
-            <InputDetail
-              label="Price"
-              type="number"
-              register={register("price")}
-              errors={errors.price}
-            />
-          </div>
+    <>
+      <form
+        method="post"
+        onSubmit={onSubmit}
+        encType="multipart/form-data"
+        className="max-[812px]:px-4"
+      >
+        <div className="border rounded-md text-muted-foreground p-4">
+          <header className="border-b pb-4 mb-10">
+            <h2 className="text-2xl font-semibold ">Announcement Details</h2>
+          </header>
+          <div className="flex flex-col gap-4 ">
+            <div className="grid grid-cols-1 gap-4 md:grid-cols-6">
+              <InputDetail
+                label="Title"
+                register={register("title")}
+                errors={errors.title}
+              />
+              <InputDetail
+                label="Description"
+                register={register("description")}
+                errors={errors.description}
+              />
+              <InputDetail
+                label="Price"
+                type="number"
+                register={register("price")}
+                errors={errors.price}
+              />
+            </div>
 
-          <div className="flex flex-col gap-4 border-b pb-4 mb-4">
-            <InputMainImg
-              register={register("mainImage")}
-              errors={errors.mainImage}
-              existingImage={existingImages.mainImage}
-            />
-            {/* Other Images */}
-            <div id="other-images" className="flex flex-col gap-4">
-              {inputImages.length > 0 &&
-                inputImages.map((image, index) => {
-                  const otherImageKey = `otherImage${index + 1}` as
-                    | "otherImage1"
-                    | "otherImage2";
-                  return (
-                    <div key={image.id}>
-                      <div
-                        key={image.id}
-                        className="w-full flex items-center gap-4"
-                      >
-                        <div className="w-full">
-                          {existingImages.otherImages[index] && (
-                            <Image
-                              src={existingImages.otherImages[index]}
-                              alt={`Image ${index + 1}`}
-                              width={100}
-                              height={100}
-                              className="object-cover mb-2"
-                            />
-                          )}
-                          <input
-                            type="file"
-                            accept="image/png, image/jpeg"
-                            className={`border w-full ${
-                              errors?.[otherImageKey] ? "border-red-500" : ""
-                            }`}
-                            {...register(otherImageKey)}
-                          />
-                        </div>
-                        <Button
-                          variant="outline"
-                          className="w-fit"
-                          onClick={(e) => handleRemoveImage(e, image.id)}
+            <div className="flex flex-col gap-4 border-b pb-4 mb-4">
+              <InputMainImg
+                register={register("mainImage")}
+                errors={errors.mainImage}
+                existingImage={existingImages.mainImage}
+              />
+              {/* Other Images */}
+              <div id="other-images" className="flex flex-col gap-4">
+                {inputImages.length > 0 &&
+                  inputImages.map((image, index) => {
+                    const otherImageKey = `otherImage${index + 1}` as
+                      | "otherImage1"
+                      | "otherImage2";
+                    return (
+                      <div key={image.id}>
+                        <div
+                          key={image.id}
+                          className="w-full flex items-center gap-4"
                         >
-                          <TrashIcon className="w-4 h-4" />
-                        </Button>
+                          <div className="w-full">
+                            {existingImages.otherImages[index] && (
+                              <Image
+                                src={existingImages.otherImages[index]}
+                                alt={`Image ${index + 1}`}
+                                width={100}
+                                height={100}
+                                className="object-cover mb-2"
+                              />
+                            )}
+                            <input
+                              type="file"
+                              accept="image/png, image/jpeg"
+                              className={`border w-full ${
+                                errors?.[otherImageKey] ? "border-red-500" : ""
+                              }`}
+                              {...register(otherImageKey)}
+                            />
+                          </div>
+                          <Button
+                            variant="outline"
+                            className="w-fit"
+                            onClick={(e) => handleRemoveImage(e, image.id)}
+                          >
+                            <TrashIcon className="w-4 h-4" />
+                          </Button>
+                        </div>
+                        <p className="text-red-500">
+                          {errors?.[otherImageKey]?.message}
+                        </p>
                       </div>
-                      <p className="text-red-500">
-                        {errors?.[otherImageKey]?.message}
-                      </p>
-                    </div>
-                  );
-                })}
-            </div>
-            <div className="flex items-center gap-4 justify-end">
-              <p className="text-sm/6">
-                Images to add ({inputImages.length}/2)
-              </p>
-              <Button
-                type="button"
-                disabled={inputImages.length >= 2}
-                className="w-fit"
-                onClick={handleAddImage}
-              >
-                <PlusIcon className="w-4 h-4" />
-              </Button>
+                    );
+                  })}
+              </div>
+              <div className="flex items-center gap-4 justify-end">
+                <p className="text-sm/6">
+                  Images to add ({inputImages.length}/2)
+                </p>
+                <Button
+                  type="button"
+                  disabled={inputImages.length >= 2}
+                  className="w-fit"
+                  onClick={handleAddImage}
+                >
+                  <PlusIcon className="w-4 h-4" />
+                </Button>
+              </div>
             </div>
           </div>
+          <div className="mt-6">
+            <CTAForm />
+          </div>
         </div>
-        <div className="mt-6">
-          <CTAForm />
-        </div>
-      </div>
-    </form>
+      </form>
+      <ToastContainer />
+    </>
   );
 }
