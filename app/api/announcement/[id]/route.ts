bc4ec7210089fa431bea0ae9996efa06eee29502
satanpticoeur <121906238+satanpticoeur@users.noreply.github.com>
2025-01-08 18:19:42 +0000

    Update route.ts

--- app/api/announcement/[id]/route.ts
+++ app/api/announcement/[id]/route.ts
@@ -1,6 +1,6 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
-import { deleteImageFromBlob } from "@/lib/blob";
+import { deleteImageFromBlob, uploadImageToBlob } from "@/lib/blob";
 
 type Params = Promise<{ id: string }>;
 
@@ -58,9 +58,9 @@ export async function PUT(request: Request, segmentData: { params: Params }) {
   const id = params.id;
   const formData = await request.formData();
 
-  // const title = formData.get("title") as string;
-  // const description = formData.get("description") as string;
-  // const price = Number(formData.get("price"));
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const price = Number(formData.get("price"));
 
   //new images
   const mainImage = formData.get("mainImage") as File | null;
@@ -75,45 +75,46 @@ export async function PUT(request: Request, segmentData: { params: Params }) {
       },
     });
     //new images array
-    // const newImages: string[] = [];
+    const newImages: string[] = [];
 
     if (existingAnnouncement) {
-      //delete old images
-      for (const oldImage of existingAnnouncement.images) {
-        if (oldImage) {
-        for (const newImage of [mainImage, otherImage1, otherImage2]) {
-          if (newImage && newImage.name === oldImage) {
-            console.log("oldImage", oldImage);
-            console.log("is the same as");            
-            console.log("newImage", newImage);
-            // await deleteImageFromBlob(oldImage);
-          }else{
-            console.log("oldImage", oldImage);
-            console.log("is not the same as");            
-            console.log("newImage", newImage);
+      let indice = 0;
+      const oldImagesArray = existingAnnouncement.images;
+      const newImagesArray = [mainImage, otherImage1, otherImage2].filter(Boolean) as File[]; 
+
+      for (const oldImage of oldImagesArray) {
+        //https://url.com/image.jpg
+          if (oldImage && newImagesArray[indice]) {
+          //c://chemin/image.jpg
+          await deleteImageFromBlob(oldImage);
+          const blob = await uploadImageToBlob(newImagesArray[indice]);
+          newImages.push(blob.url);
+
+          } else if (!oldImage && newImagesArray[indice]) {
+            const blob = await uploadImageToBlob(newImagesArray[indice]);
+            newImages.push(blob.url);
           }
-          }
-        }
+        indice++;
       }
-      // //upload new images
-      // for (const image of [mainImage, otherImage1, otherImage2]) {
+      //upload new images
+      // for (const image of newImagesArray) {
       //   if (image) {
-      //     const blob = await uploadImageToBlob(image);
+      //     const blob = await uploadImageToBlob(image as File);
       //     newImages.push(blob.url);
       //   }
       // }
-      // //update announcement
-      // const updatedAnnouncement = await prisma.announcement.update({
-      //   where: { id },
-      //   data: {
-      //     title,
-      //     description,
-      //     price,
-      //     images: newImages,
-      //   },
-      // });
+      //update announcement
+      const updatedAnnouncement = await prisma.announcement.update({
+        where: { id },
+        data: {
+          title,
+          description,
+          price,
+          images: newImages,
+        },
+      });
 
-      return NextResponse.json({ message: "Announcement updated" });
+      return NextResponse.json(updatedAnnouncement);
     }
     return NextResponse.json(
       { error: "Announcement not found" },
